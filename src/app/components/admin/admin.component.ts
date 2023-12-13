1040923c6a5a377f7384723001ace86587a3ce80
GiuseppeBarresi98 <145044894+GiuseppeBarresi98@users.noreply.github.com>
2023-12-13 17:10:21 +0100

    3rd

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent implements OnInit {
+  users: any[] = [];
+  posts: any[] = [];
+  selectedUserId!: number;
 
-  constructor() { }
+  constructor(private userSrv: UserService) {}
 
   ngOnInit(): void {
+    this.loadUser();
+    this.getpost();
   }
 
+  loadUser() {
+    this.userSrv.getUsers().subscribe((users) => {
+      this.users = users;
+      console.log(this.users);
+    });
+  }
+
+  selectUser(userId: number) {
+    this.selectedUserId = userId;
+  }
+
+  deleteUser(userId: number): void {
+    this.userSrv.delUser(userId).subscribe(() => {
+      this.loadUser();
+    });
+  }
+
+  getpost() {
+    this.userSrv.getPosts().subscribe((post) => {
+      this.posts = post;
+      console.log(post);
+    });
+  }
 }
